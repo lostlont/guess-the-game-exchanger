651f01a789551be3cf0bf9e04ce69c08ab205690
Perjéssy, Lóránt <lostlont@users.noreply.github.com>
2023-03-04 23:19:18 +0100

    Using iced package for UI.

--- crates/app/src/main.rs
+++ crates/app/src/main.rs
@@ -1,4 +1,32 @@
-fn main()
+use iced::{Element, Sandbox, Settings};
+
+pub fn main() -> iced::Result
 {
-	println!("Hello, world!");
+	Hello::run(Settings::default())
+}
+
+struct Hello;
+
+impl Sandbox for Hello
+{
+	type Message = ();
+
+	fn new() -> Hello
+	{
+		Hello
+	}
+
+	fn title(&self) -> String
+	{
+		String::from("Guess The Game Exchanger")
+	}
+
+	fn update(&mut self, _message: Self::Message)
+	{
+	}
+
+	fn view(&self) -> Element<Self::Message>
+	{
+		"Hello, world!".into()
+	}
 }
